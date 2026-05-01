576b52c085d9e429acd362ba0909fa55bdea5b36
Jacob GLZ <89885702+g17dev@users.noreply.github.com>
2026-05-01 16:32:12 -0700

    feat: agregar componente BiometricHandoverCapture a AuthorizePickupStep y darle primeros estilos con ciertas variables de comportamiento que falta programar

--- src/features/child-handover/components/BiometricHandoverCapture.tsx
+++ src/features/child-handover/components/BiometricHandoverCapture.tsx
@@ -1,10 +1,70 @@
 import "./BiometricHandoverCapture.css"
+import { ScanFace } from "lucide-react";
+import { FingerprintPattern } from "lucide-react"
 
 export const BiometricHandoverCapture = () => {
 
     return (
-        <div>
-            HOLA
+        <div className="biometric-handover-capture">
+            <div className="camera-box">
+                <video
+                    autoPlay
+                    playsInline
+                    muted
+                    className="camera-video"
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                        display: 'block'
+                    }}
+                />
+
+                <div className="scanface-overlay">
+                    <ScanFace size={300} color="#004B92" strokeWidth={1} />
+                    <p className={`status ${false ? 'success' : ''}`}>
+                        {/* {getDisplayMessage()} */}
+                        Buscando camara...
+                        {/* Conectando con el servidor... */}
+                        {/* Solicitando permisos de cámara... */}
+                        {/* Iniciando camara... */}
+                    </p>
+                </div>
+
+                <div className="face-detected-indicator">
+                    <span>✓ Rostro detectado</span>
+                    {/* <span>⚠️ Acomoda tu rostro en el marco</span> */}
+                </div>
+
+            </div>
+
+            <div className="fingerprint-section">
+                <div className="fingerprint-card">
+                    {/* Icono de huella */}
+                    <div className="fingerprint-icon-wrapper">
+                        <FingerprintPattern 
+                            size={64} 
+                            strokeWidth={1.5}
+                            className="fingerprint-icon deactive"
+                        />
+                    </div>
+
+                    {/* Texto y descripción */}
+                    <div className="fingerprint-info">
+                        <h3 className="fingerprint-title">Verificación de huella</h3>
+                        <p className="fingerprint-description">
+                            Espere un momento, mientras estamos verificando el sensor de huella...
+                            {/* Sensor listo, por favor inicie el escaneo para confirmar su identidad. */}
+                        </p>
+                    </div>
+
+                    {/* Botón de acción */}
+                    <button className="fingerprint-button deactive">
+                        Iniciar Escaneo de Huella
+                        {/* Escaneando... */}
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
